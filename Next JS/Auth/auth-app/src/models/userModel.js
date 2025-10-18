ff8b75b1e90071854236dbafc8aec5a605d49c12
uvpatel <uvpatel7271@gmail.com>
2025-10-18 21:34:34 +0530

    Worked

--- Next JS/Auth/auth-app/src/models/userModel.js
+++ Next JS/Auth/auth-app/src/models/userModel.js
@@ -1,35 +1,34 @@
-import moongoose from "moongoose"
+import mongoose from "mongoose";
 
-const userSchema  = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true,"Please provide a username"],
-        unique: true
+        required: [true, "Please provide a username"],
+        unique: true,
     },
     email: {
         type: String,
-        required: [true,"Please provide a email"],
-        unique: true
+        required: [true, "Please provide a email"],
+        unique: true,
     },
     password: {
         type: String,
-        required: [true,"Please provide a Password"]
+        required: [true, "Please provide a password"],
     },
     isVerfied: {
         type: Boolean,
-        default: false  
+        default: false,
     },
     isAdmin: {
         type: Boolean,
-        default: false
+        default: false,
     },
     forgotPasswordToken: String,
     forgotPasswordTokenExpiry: Date,
     verifyToken: String,
-    verifyTokenExpiry: Date
+    verifyTokenExpiry: Date,
 })
 
+const User = mongoose.models.users || mongoose.model("users", userSchema);
 
-const User = moongoose.models.users || mongoose.model("users",userSchema)
-
-export default User
+export default User;
